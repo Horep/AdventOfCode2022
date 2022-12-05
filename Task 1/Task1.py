9f6a7b5fcd0d25acabdd7975e1daa2a11cf4a1e8
Horep <horep245@gmail.com>
2022-12-05 18:33:23 +0000

    completed the second half of task

--- Task 1/Task1.py
+++ Task 1/Task1.py
@@ -11,5 +11,6 @@ for i in range(1,len(num)-1):  # separate out lists using '' as marker
     ElfCalorie.append(Lines[num[i]+1:num[i+1]])
 ElfCalorie = [list(map(int, Food)) for Food in ElfCalorie]  # typecast to integers
 SumList = [sum(Food) for Food in ElfCalorie]  # sum up individual lists
-print(max(SumList))
-
+print(f"The elf with the most calories has {max(SumList)} calories.")
+SortedSumList = sorted(SumList)[len(SumList)-3:len(SumList)+1]
+print(f"The top three elves combined have {sum(SortedSumList)} calories.")
